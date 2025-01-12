874f8061e70b03f194f8ae921e727ae758b87624
gpzeke <65458253+gpzeke@users.noreply.github.com>
2025-01-11 20:38:44 -0600

    added markdown to blocks, started ch4

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
 from textnode import TextNode, TextType
 from htmlnode import *
 from inline_markdown import *
+from block_markdown import *
 
 def main():
-    pass
+    text = """# This is a heading
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item"""
+    blocks = markdown_to_blocks(text)
+    for item in blocks:
+        print(f"{item}\n")
+
+    print(f'{type(blocks)} of length: {len(blocks)}')
 
 if __name__ == "__main__":
     main()

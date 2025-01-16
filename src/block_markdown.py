7d970ff54d9e2edb86b5c96ef45d497bf3464733
gpzeke <65458253+gpzeke@users.noreply.github.com>
2025-01-15 20:16:13 -0600

    Finished blocks to markdown

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,12 @@
+import re
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_olist = "ordered_list"
+block_type_ulist = "unordered_list"
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
 
@@ -9,3 +18,62 @@ def markdown_to_blocks(markdown):
         filtered_blocks.append(block)
     
     return filtered_blocks
+
+def block_to_block_type(block):
+    leading_character = block[0]
+    
+    # process headings
+    if leading_character == '#':
+        lead_number = len(block) - len(block.lstrip(leading_character))
+        if lead_number <= 6:
+            return block_type_heading
+            #return f'<h{lead_number}>'
+    
+    # process code blocks
+    if leading_character == '`':
+        if block[:3] == '```':
+            if block[-3:] == '```':
+                return block_type_code
+                #return '<code>'
+    
+    # process blockquotes
+    if leading_character == '>':
+        lines = block.splitlines()
+        valid_quote = []
+        for line in lines:
+            valid_quote.append(line[0] == '>')
+
+        result = all(valid_quote)
+        if result:
+            return block_type_quote
+            #return '<blockquote>'
+    
+    # process unordered lists
+    ul_characters = ['-', '*']
+    if leading_character in ul_characters:
+        lines = block.splitlines(ul_characters)
+        valid_list_items = []
+        for line in lines:
+            valid_list_items.append(line[0] in ul_characters)
+        
+        result = all(valid_list_items)
+        if result:
+            return block_type_ulist
+            #return '<ul>'
+    
+    # process ordered lists
+    regex = r'\d+\.+ '
+    
+    if bool(re.match(regex, block)):
+        lines = block.splitlines()
+        valid_list_items = []
+        for line in lines:
+            valid_list_items.append(bool(re.match(regex, line)))
+        
+        result = all(valid_list_items)
+        if result:
+            return block_type_olist
+            #return '<ol>'
+
+    return block_type_paragraph
+    #return '<p>'
--- src/main.py
+++ src/main.py
@@ -4,18 +4,12 @@ from inline_markdown import *
 from block_markdown import *
 
 def main():
-    text = """# This is a heading
-
-This is a paragraph of text. It has some **bold** and *italic* words inside of it.
-
-* This is the first list item in a list block
-* This is a list item
-* This is another list item"""
-    blocks = markdown_to_blocks(text)
-    for item in blocks:
-        print(f"{item}\n")
-
-    print(f'{type(blocks)} of length: {len(blocks)}')
+    text = """1. test 1
+2. test 2
+3. test 3
+4. test 4"""
+    result = block_to_block_type(text)
+    print(result)
 
 if __name__ == "__main__":
     main()
